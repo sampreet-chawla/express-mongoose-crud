e46fe9952f9972866012f232bf50e7f8edc3ec4a
Joe Keohan <jkeohan@gmail.com>
2020-10-16 16:18:08 -0400

    added put route for user to update fruits

--- instructor_examples/fruits/controllers/owner.js
+++ instructor_examples/fruits/controllers/owner.js
@@ -6,12 +6,27 @@ const router = express.Router();
 const mongoose = require('../db/connection');
 // import the Fruit model
 const Owner = require('../models/owner');
+const Fruit = require('../models/fruit')
 
 router.post('/', async (req, res) => {
     console.log('owner-post', req.body)
     const owner = await Owner.create(req.body)
     res.json({ status: 200, data: owner})
 })
+
+// ownerId: 5f89fea7c6a6780d4074852b - sara
+// fruitId: 5f89ec2a6188f001527d2fed - apple
+router.put('/:ownerId/addFruits/:fruitId', async (req,res) => {
+    console.log('owner - put', req.params)
+    const fruit = await Fruit.findById(req.params.fruitId)
+    const owner = await Owner.findByIdAndUpdate(
+            req.params.ownerId,
+            { $push: {fruits: fruit.id}, new: true }
+    )
+    res.json({status: 200, data: owner})
+})
+
+
 
 // export router
 module.exports = router
